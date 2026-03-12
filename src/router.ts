d12b1b5409f81ce3b99ff29444fa888c80e7b41b
hardijs kivers <hardijs.kivers@whitedigital.eu>
2026-03-12 13:14:01 +0200

    init

--- src/router.ts
+++ src/router.ts
@@ -1,16 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from './views/Home.vue'
-import Create from './views/Create.vue'
-import Edit from './views/Edit.vue'
-import Packs from './views/Packs.vue'
 import Privacy from './views/Privacy.vue'
 
 const routes = [
-  { path: '/', component: Home },
-  { path: '/create', component: Create },
-  { path: '/packs', component: Packs },
-  { path: '/edit', component: Edit },
-  { path: '/edit/:token', component: Edit },
+  { path: '/', redirect: '/privacy' },
   { path: '/privacy', component: Privacy },
 ]
 
